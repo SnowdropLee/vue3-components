4e886b31d1957019cf0b9384af4eac43f84bbd24
lilifu <lilifu@agree.com.cn>
2020-08-03 10:12:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Topnave />
+  <Home />
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import Topnave from "./components/Topnave.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    Topnave,
+  },
+};
+</script>
