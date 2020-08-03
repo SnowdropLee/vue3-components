6f11db046c05f9c9a4ae540851e0eb9d67e9547d
lilifu <lilifu@agree.com.cn>
2020-08-03 15:52:31 +0800

    Doc 页面样式调整

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     <Topnave />
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
     <div class="content">
-        <aside>边栏</aside>
-        <main>主内容</main>
+      <aside>
+        <h2>组件列表</h2>
+        <ul>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ul>
+      </aside>
+      <main>主内容</main>
     </div>
   </div>
 </template>
@@ -19,3 +31,22 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+</style>
