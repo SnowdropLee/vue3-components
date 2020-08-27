359a63fac43f8a49a590257925f000931a1e938d
snowdrop <l522347325@qq.com>
2020-08-27 16:04:35 +0800

    增加visible属性

--- src/components/Dialogdoc.vue
+++ src/components/Dialogdoc.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <Dialog v-model:visible="visible"></Dialog>
   </div>
 </template>
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
 export default {
   components: {
     Dialog,
+    Button,
+  },
+  setup(props, context) {
+    const visible = ref(false);
+    const toggle = () => {
+      visible.value = !visible.value;
+
+    };
+    return { visible, toggle };
   },
 };
 </script>
