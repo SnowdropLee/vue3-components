5fa4d4ced7b7891c174c5082e74324bad90880df
snowdrop <l522347325@qq.com>
2020-08-27 11:37:02 +0800

    dialog init

--- src/components/Dialogdoc.vue
+++ src/components/Dialogdoc.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
-        Dialog
-    </div>
+  <div>
+    <Dialog></Dialog>
+  </div>
 </template>
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: {
+    Dialog,
+  },
+};
+</script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-        
+  <div>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+      <header>标题</header>
+      <main>
+        <p>第一行</p>
+        <p>二行</p>
+      </main>
+      <footer>
+        <Button>ok</Button>
+        <Button>cancle</Button>
+      </footer>
     </div>
+  </div>
 </template>
-<script>
+<script lang="ts">
+import Button from "./Button.vue";
 export default {
-    
-}
+  components: { Button },
+};
 </script>
 <style lang="scss">
-    
 </style>
